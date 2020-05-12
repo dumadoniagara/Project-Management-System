2281b8f9ecc5dfce63079134da3e33e4e7d1155b
dumadoniagara <dumadsambora@gmail.com>
2020-05-13 00:06:40 +0700

    update session, middleware, and flash

--- routes/index.js
+++ routes/index.js
@@ -2,46 +2,60 @@ var express = require('express');
 var router = express.Router();
 const bcrypt = require('bcrypt');
 
+function isLoggedIn(req, res, next) {
+  if (req.session.user) {
+    next();
+  } else {
+    res.redirect('/');
+  }
+}
 
 /* GET home page. */
 module.exports = (db) => {
   router.get('/', function (req, res, next) {
-    res.render('login');
+    res.render('login',{messages : req.flash('loginInfo')});
   });
 
   router.post('/login', (req, res) => {
     const sql = 'SELECT * FROM users WHERE email = $1'
     const email = [req.body.email];
     db.query(sql, email, (err, data) => {
-      if (err) return res.status(500).json({
-        error: true,
-        message: "error di query"
-      })
-      if (data.rows.length == 0) return res.status(500).json({
-        error: true,
-        message: `email doesn't exist`
-      })
-      console.log(data.rows[0])
+      if (err) {
+        req.flash('loginInfo', 'Terjadi kesalahan, silahkan coba lagi nanti');
+        return res.redirect('/');
+      }
+      if (data.rows.length == 0) {
+        req.flash('loginInfo', 'email or password wrong');
+        return res.redirect('/');
+      }
       bcrypt.compare(req.body.password, data.rows[0].password, function (err, result) {
-        if (err) return res.status(500).json({
-          error: true,
-          message: "error di bcrypt"
-        })
-        if (result){
-          res.redirect('/projects');
+        if (err) {
+          req.flash('loginInfo', 'terjadi kesalahan, silahkan coba lagi nanti');
+          return res.redirect('/');
+        }
+        if (result) {
+          req.session.user = data.rows[0];
+          res.render('projects', { user: req.session.user });
         } else {
+          req.flash('loginInfo', 'email or password wrong');
           res.redirect('/');
         }
       })
     });
   })
 
-  router.get('/projects', function (req, res, next) {
-    res.render('projects');
+  router.get('/projects', isLoggedIn, function (req, res, next) {
+    res.render('projects', { user: req.session.user });
   });
 
-  router.get('/profile', function(req,res){
+  router.get('/profile', isLoggedIn, function (req, res) {
     res.render('profile');
+  })
+
+  router.get('/logout', (req, res) => {
+    req.session.destroy(function (err) {
+      res.redirect('/');
+    })
   })
 
   return router;
